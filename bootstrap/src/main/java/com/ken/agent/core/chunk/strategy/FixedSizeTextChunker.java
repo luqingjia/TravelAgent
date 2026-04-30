9bc5e66fd1dbd2fb1b46c20c49cd360b06ce9187
luqj <ken1433883954@163.com>
2026-04-30 17:46:01 +0800

    feature(chunk):初步构建分块模块

--- bootstrap/src/main/java/com/ken/agent/core/chunk/strategy/FixedSizeTextChunker.java
+++ bootstrap/src/main/java/com/ken/agent/core/chunk/strategy/FixedSizeTextChunker.java
@@ -1,28 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.nageoffer.ai.ragent.core.chunk.strategy;
+package com.ken.agent.core.chunk.strategy;
 
 import cn.hutool.core.util.IdUtil;
-import com.nageoffer.ai.ragent.core.chunk.ChunkingMode;
-import com.nageoffer.ai.ragent.core.chunk.ChunkingOptions;
-import com.nageoffer.ai.ragent.core.chunk.ChunkingStrategy;
-import com.nageoffer.ai.ragent.core.chunk.FixedSizeOptions;
-import com.nageoffer.ai.ragent.core.chunk.VectorChunk;
+import com.ken.agent.core.chunk.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -43,8 +22,8 @@ import java.util.List;
 public class FixedSizeTextChunker implements ChunkingStrategy {
 
     @Override
-    public ChunkingMode getType() {
-        return ChunkingMode.FIXED_SIZE;
+    public ChunkingEnum getChunkingStrategyEnum() {
+        return ChunkingEnum.FIXED_SIZE;
     }
 
     @Override
