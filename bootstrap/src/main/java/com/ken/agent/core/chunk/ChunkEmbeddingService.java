c5f0dff76eefaf37b4a3f53fde43135c88d74894
luqj <ken1433883954@163.com>
2026-05-06 17:59:15 +0800

    feature(chunking):完成文档切分策略工厂的构建，以及模型接口的测试

--- bootstrap/src/main/java/com/ken/agent/core/chunk/ChunkEmbeddingService.java
+++ bootstrap/src/main/java/com/ken/agent/core/chunk/ChunkEmbeddingService.java
@@ -1,10 +1,15 @@
 package com.ken.agent.core.chunk;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.stereotype.Service;
 
 // TODO
 @Service
 @RequiredArgsConstructor
 public class ChunkEmbeddingService {
+    private EmbeddingModel embeddingModel;
+
+    
+
 }
